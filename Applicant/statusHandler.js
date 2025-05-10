d7035ede4a169e65229fd09ffca04ca7b008ed3e
priskaaimee <likarsaaimee@gmail.com>
2025-05-10 18:24:21 +0700

    allow applicants to view status updated by admin

--- Applicant/statusHandler.js
+++ Applicant/statusHandler.js
@@ -1,20 +1,20 @@
 // statusHandler.js
 export function handleStatus(status) {
-    switch (status) {
-      case 'Submitted':
-      case 'Reviewed':
-      case 'Additional Documents Requested':
-      case 'Rejected':
-      case 'Approved':
-        return {
-          displayStatus: status,
-          statusClass: `application-status ${status.toLowerCase().replace(" ", "-")}`
-        };
-      default:
-        return {
-          displayStatus: 'In Progress',
-          statusClass: 'application-status in-progress'
-        };
-    }
+  switch (status) {
+    case 'Submitted':
+    case 'Reviewed':
+    case 'Additional Documents Requested':
+    case 'Reject':
+    case 'Approve':
+    case 'Waitlist':
+      return {
+        displayStatus: status,
+        statusClass: `application-status ${status.toLowerCase().replace(" ", "-")}`
+      };
+    default:
+      return {
+        displayStatus: 'In Progress',
+        statusClass: 'application-status in-progress'
+      };
   }
-  
+}
